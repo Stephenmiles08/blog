0dd3ce0bc5179cdf6ee7bc597cbe88a12bb4ad8f
stephen_miles <stephenuchechukwu68@gmail.com>
2022-12-26 20:36:22 +0100

    fixed some error with the like and unlike function

--- controller/like.controller.js
+++ controller/like.controller.js
@@ -10,7 +10,8 @@ exports.like = async(req,res)=>{
         });
         if (uploadLike[1] === false)
                 throw new Exception(`user with id: ${userId} Can not like`,400) 
-        const likePost = await models.posts.findOne({where:{id:postId,userId}});
+        const likePost = await models.posts.findOne({where:{id:postId}});
+        
         await likePost.increment('likeCount',{by:1});
         successResponse(res,{success:true},200);
     } catch (error) {
@@ -25,7 +26,9 @@ exports.unlike = async(req,res)=>{
         const deletedLike = await models.likes.destroy({where:{userId,postId}});
         if (deletedLike !== 1)
             throw new Exception('Unable to delete')
-        const unLikePost = await models.posts.findOne({where:{id:postId,userId}});
+        const unLikePost = await models.posts.findOne({where:{id:postId}});
+        console.log(unLikePost);
+        if (unLikePost.likeCount >1)
         await unLikePost.decrement('likeCount',{by:1});
         successResponse(res,{success:true},200);
     } catch (error) {
